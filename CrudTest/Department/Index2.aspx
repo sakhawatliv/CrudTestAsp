a69acf456039f1b740ec256631a7112462ae0ef9
sakhawatliv <sakhawatliv@gmail.com>
2019-02-06 17:31:14 +0600

    index 2 added

---
+++ CrudTest/Department/Index2.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Index2.aspx.cs" Inherits="CrudTest.Department.Index2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Table ID="Table1" runat="server"></asp:Table>
+</asp:Content>
---
+++ CrudTest/Department/Index2.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CrudTest.Department
+{
+    public partial class Index2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            string query = null;
+
+            connectionString = @"Data Source=DESKTOP-1HKAK02\SQLEXPRESS;Initial Catalog=CrudTest;Integrated Security = true";
+            query = "SELECT * FROM Department";
+            connection = new SqlConnection(connectionString);
+            command = new SqlCommand(query,connection);
+
+            adapter.SelectCommand = command;
+            adapter.Fill(ds, "Department");
+            adapter.Dispose();
+            connection.Dispose();
+            connection.Close();
+
+            foreach (DataTable table in ds.Tables)
+            {
+                foreach (DataRow row in table.Rows)
+                {
+                    foreach (DataColumn col in table.Columns)
+                    {
+                        Response.Write(row[col]);
+                    }
+
+                }
+
+
+            }
+
+        }
+    }
+}
